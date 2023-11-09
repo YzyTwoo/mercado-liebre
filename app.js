d30dd35926a84438258079f257d64ca7ca5bede2
Tiziano Orieta <tiziorieta@gmail.com>
2023-11-09 18:32:58 -0300

    estructura html en formularios terminada

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const express = require('express');
 const app = express();
 const path = require('path')
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname, "/views/home.html"))
-})
+});
 
-app.use(express.static('public'))
+app.get("/crea-tu-cuenta", (req, res)=>{
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+});
+
+app.get("/ingresa", (req, res)=>{
+    res.sendFile(path.join(__dirname, "/views/login.html"))
+});
+
+app.use(express.static('public'));
 
 app.listen(3030,()=>{
     console.log("Servidor corriendo en puerto 3030");
-})
+});
