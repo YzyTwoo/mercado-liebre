23fc232d64594ace7e7d9a83cc75a5686f084b49
Tiziano Orieta <tiziorieta@gmail.com>
2023-11-14 08:44:57 -0300

    method post agregado en los forms

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path')
+const bodyParser = require('body-parser');
+
+app.use(express.static(path.join(__dirname,'./public')))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res)=>{
     res.sendFile(path.join(__dirname, "/views/home.html"))
@@ -12,9 +17,18 @@ app.get("/crea-tu-cuenta", (req, res)=>{
 
 app.get("/ingresa", (req, res)=>{
     res.sendFile(path.join(__dirname, "/views/login.html"))
+    console.log(__dirname)
 });
 
-app.use(express.static('public'));
+app.post('/crea-tu-cuenta', (req,res) => {
+	console.log (req.body);
+	res.redirect('/ingresa');
+});
+
+app.post('/ingresa', (req,res) => {
+	console.log (req.body);
+	res.redirect('/');
+});
 
 app.listen(3030,()=>{
     console.log("Servidor corriendo en puerto 3030");
